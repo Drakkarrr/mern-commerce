98f13b8b45a8e5f65aaa8bb9963ba019dbaabc32
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-06 06:59:26 -0800

    API created

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.get('/', (req, res) => {
   res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
