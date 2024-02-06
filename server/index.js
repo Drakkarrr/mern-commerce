93e2ca48f9d70616ddf5ba369e58a03b243e55b9
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-06 06:47:23 -0800

    basic imports

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log('Server-side code is now running');
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import connectDB from './config/connectDb.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
