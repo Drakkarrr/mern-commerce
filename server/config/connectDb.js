9f511f0fdfd898b06261ae66f794af3b6b90e1ff
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-06 06:52:45 -0800

    created conn for db local and made a live server

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -2,11 +2,7 @@ import mongooes from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongooes.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongooes.connect(process.env.MONGO_URI);
 
     console.log(`MongoDb connected: ${conn.connection.host}`);
   } catch (error) {
@@ -14,3 +10,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+export default connectDB;
