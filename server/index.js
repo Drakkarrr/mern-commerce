3b9781ff35fb0f62d860af2d3ab4a406736038ca
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-21 10:10:33 -0800

    complete api routes enpoints. ready for testing.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
-// import path from 'path';
+// packages
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
-import connectDB from './config/connectDb.js';
+// Utiles
+import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -18,7 +24,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/orders', orderRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/api/config/paypal', (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
